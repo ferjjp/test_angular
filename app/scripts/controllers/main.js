2882db67f80e099b463b9f35ea2412f14824581d
Andres Kirschbaum <andres@parsimotion.com>
2014-09-29 18:05:54 -0300

    creado método saveAll en mainCtrl

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,37 @@
 'use strict';
 
 angular.module('todoAppApp')
-  .controller('MainCtrl', function ($scope, taskRepository) {
+  .controller('MainCtrl', function ($scope, taskRepository, Task) {
 
     $scope.tasks = taskRepository.get();
 
     $scope.addTask = function(){
-      var newTask = {date: new Date().toISOString(), isDirty: true};
+      var newTask = new Task({date: new Date().toISOString()});
       $scope.tasks.push(newTask);
       return newTask;
+    };
+
+    $scope.hasChanged = function(){
+      return _.some($scope.tasks, function(task){
+        return task.isDirty() || task.toBeDeleted;
+      });
+    };
+
+    $scope.saveAll = function(){
+      $scope.tasks.forEach(function(task){
+
+        if(task.isNew()){
+          taskRepository.add(task);
+        }
+        else if(task.isDirty() && !task.toBeDeleted){
+          taskRepository.update(task);
+        }
+        else if(task.toBeDeleted){
+          taskRepository.delete(task);
+        }
+
+      });
+      $scope.tasks = taskRepository.get();
     };
 
   });
